055fcdc2eb67e222b8e6143a7ffebdc3908f2aae
Unnukjaak <134617917+Unnukjaak@users.noreply.github.com>
2023-05-25 16:25:36 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,60 @@
+package org.example;
+
+public class User extends UserDataBase {
+    private String userName;
+    private String password;
+    private String fullName;
+    private String email;
+    private Integer userID;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", userID=" + userID +
+                '}';
+    }
+}
